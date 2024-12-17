414d0ba4bf8db899ee792d62864048ce53d7cf41
kazu <test@gmail.com>
2024-12-17 21:50:22 +0900

    二回目の変更をする！！！！

--- src/main/java/dao/ProductDAO.java
+++ src/main/java/dao/ProductDAO.java
@@ -43,7 +43,7 @@ public class ProductDAO extends DAO {
 		st.setInt(2, product.getPrice());
 		int line=st.executeUpdate();
 
-		//一回目の変更をする！！！！
+		//二回目の変更をする！！！！
 		st.close();
 		con.close();
 		return line;
